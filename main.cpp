fec1939324f8374982b77b7d0f4157a2f718e622
if13b023 <if13b023@technikum-wien.at>
2017-10-22 03:30:13 +0200

    clean up

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <new>
 #include <iostream>
-//#include <vector>
 
 template <class T>
 class vector
@@ -160,9 +159,30 @@ int main()
 	}
 	
 	std::cout << "reserve test finished" << std::endl;
-	std::cin.ignore();
 	//*** rt
 
+	//resize test
+	std::cout << std::endl;
+		//expected output
+		//myvector contains: 1 2 3 4 5 100 100 100 0 0 0 0
+	vector<int> myvector;
+
+	// set some initial content:
+	for (int i = 1; i<10; i++) myvector.push_back(i);
+
+	myvector.resize(5);
+	myvector.resize(8, 100);
+	myvector.resize(12);
+
+	std::cout << "myvector contains:";
+	for (int i = 0; i<myvector.size(); i++)
+		std::cout << ' ' << myvector[i];
+	std::cout << std::endl;
+	std::cout << "resize test finished" << std::endl;
+	//*** rt
+
+	//own test
+	std::cout << std::endl;
 	vector<double>* ownIntVec = new vector<double>();
 
 	std::cout << "size:" << ownIntVec->size() << "/" << ownIntVec->capacity() << std::endl;
@@ -174,7 +194,10 @@ int main()
 		std::cout << (*ownIntVec)[i] << std::endl;
 
 	std::cout << "size:" << ownIntVec->size() << "/" << ownIntVec->capacity() << std::endl;
-	std::cin.ignore();
 
 	delete ownIntVec;
+
+	std::cout << "press ENTER to exit\n";
+	std::cin.ignore();
+	//*** own test
 }
